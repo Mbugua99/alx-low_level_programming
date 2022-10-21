45fca72c96018eb8471f615fe65b5dc4aa44b6f5
Mbugua <mbugua@DESKTOP-0296S7L.localdomain>
2022-10-21 23:18:53 +0300

    important

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-#include "main.h"
+nclude "main.h"
 
 /**
  * print_last_digit - prints the last digit
@@ -7,12 +7,12 @@
  */
 int print_last_digit(int n)
 {
-int ld = (n % 10);
+	int ld = (n % 10);
 
-if (ld < 0)
-	ld *= (-1);
+	if (ld < 0)
+		ld *= (-1);
 
-_putchar(ld + '0');
+	_putchar(ld + '0');
 
-return (0);
+	return (0);
 }
