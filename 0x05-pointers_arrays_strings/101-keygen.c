ce2e5295e4ef4ef5f98763b3e7203de114378255
Mbugua <mbugua@DESKTOP-0296S7L.localdomain>
2022-10-26 00:33:32 +0300

    Dont hate the hacker hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,10 +1,10 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
  * main - generate random valid password
+ *
  * Return: Always 0
  */
 int main(void)
@@ -29,4 +29,6 @@ int main(void)
 			break;
 		}
 	}
+
+	return (0);
 }
